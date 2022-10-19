43614444696459f10db9e5ce83e7e1d7f84720cd
MackenzieC0llins <113036337+MackenzieC0llins@users.noreply.github.com>
2022-10-19 09:25:32 -0400

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,22 @@
+sort1 uses: I think that sort1 uses a Bubble Sort.
+
+How do you know?: I know this because on a sorted list, it ran the fastest making it a bubble sort.
+
+sort2 uses: I think that sort2 uses Merge Sort.
+
+How do you know?: I know this because on an unsorted list, it ran the fastest making it a merge sort.
+
+sort3 uses: I think that sort3 uses a Selection Sort.
+
+How do you know?: I know this because on a sorted list, it ran the slowest making it a selection sort.
+
+Results
+Unsorted lists:
+sort1- 7.871 seconds
+sort2- 0.663 seconds
+sort3- 3.519 seconds
+
+Sorted lists:
+sort1- 0.440 seconds
+sort2- 0.577 seconds
+sort3- 3.156 seconds
